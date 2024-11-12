0525149f6c3d4f4dd5aed29f0a380a75ecd11d14
dave2342 <david.varella@snhu.edu>
2024-11-11 22:53:14 -0500

    Limit FPS to 60; Create base class for objects; Draw Player

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
-	pygame.init
+	#Initialize imported pygame modules
+	pygame.init()
+	
+	#set up the game screen with height and width
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	#log info
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	#create a clock object to manage screen updates
+	clock = pygame.time.Clock()
+	
+	#delta time variable
+	dt = 0
 
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  
+	
+	#game loop
 	while True:
+		#process all events in game
 		for event in pygame.event.get():
+			#if QUIT event(such as ctrl+c, or X in program screen, exit the game
 			if event.type == pygame.QUIT:
-				return
-		screen.fill(000000)
+				pygame.quit()
+				sys.exit()
+
+		#clear screen and fill with black color
+		screen.fill("black")
+
+		#draw the screen
+		player.draw(screen)
+
+		#update the display
 		pygame.display.flip()
 
+	#control the loop's speed to 60fps and calculate delta time (divide by 1000 to get milliseconds)
+	dt = clock.tick(60) / 1000
+
+#ensures main is called only when this screen is directly executed
 if __name__ == "__main__":
 	main()
 
