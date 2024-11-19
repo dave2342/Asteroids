4f7567f83ae567e94ee6d571aabd91faac060edb
dave2342 <david.varella@snhu.edu>
2024-11-18 21:31:54 -0500

    Added shooting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 from constants import *
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
+from shot import Shot
 import sys
 
 def main():
@@ -17,10 +18,12 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 
 	Player.containers = (updatable, drawable)  
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = updatable
+	Shot.containers = (shots, updatable, drawable)
 
 	#log info
 	print("Starting asteroids!")
--- player.py
+++ player.py
@@ -1,12 +1,15 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
+PLAYER_SHOOT_COOLDOWN = 0.3
 
 class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.timer = 0
     
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +26,8 @@ class Player(CircleShape):
 		self.rotation += (PLAYER_TURN_SPEED * dt)
 
 	def update(self, dt):
+		self.timer = max(0, self.timer -dt)
+
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_a]:
@@ -35,7 +40,21 @@ class Player(CircleShape):
 			self.move(dt)
 		if keys[pygame.K_s]:
 			self.move(-dt)
+		if keys[pygame.K_SPACE]:
+			self.shoot()
 
 	def move(self, dt):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
 		self.position += forward * PLAYER_MOVE_SPEED * dt
+
+	def shoot(self):
+		if self.timer <= 0:
+			shot = Shot(self.position.x, self.position.y)
+		#(0, 1) points down, y-axis flipped in comp graphics
+			velocity = pygame.Vector2(0, 1)
+		# negative rotation because pygame rotation is clockwise, but we want counterclockwise to flip
+			velocity = velocity.rotate(-self.rotation)
+			velocity *= PLAYER_SHOOT_SPEED
+			shot.velocity = velocity
+			self.timer = PLAYER_SHOOT_COOLDOWN
+
